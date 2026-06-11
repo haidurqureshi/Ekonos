2ebe8577b29957fe9358bb6ff954e6502f471376
haidurqureshi <haidurqureshi@gmail.com>
2026-06-11 16:44:48 +0100

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import { redirect } from "next/dist/client/components/navigation";
+import { cookies } from "next/dist/server/request/cookies";
 import Image from "next/image";
 
-export default function Home() {
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token')?.value;
+
+  if (token) {
+    redirect('/dashboard');
+  }
+  
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-15 px-16 bg-white dark:bg-black sm:items-start">
