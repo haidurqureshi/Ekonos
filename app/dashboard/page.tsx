e4c7caa5b543986421c74d288df06477eb32393b
haidurqureshi <haidurqureshi@gmail.com>
2026-06-11 16:59:23 +0100

    Add files via upload

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -46,7 +46,9 @@ export default async function Dashboard() {
 
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <p >Logout</p>
+            <p onClick={handleSubmit} className="cursor-pointer">
+                Logout
+            </p>
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-15 px-16 bg-white dark:bg-black sm:items-start">
                 <Image 
                 src="/EKONOS.svg" 
