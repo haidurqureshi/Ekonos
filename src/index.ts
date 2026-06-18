ca748e3e9c84c9ec4f6bea3559c3643f64aba756
haidurqureshi <haidurqureshi@gmail.com>
2026-06-18 12:06:14 +0100

    Delete src directory

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request: Request) {
-    return new Response("Ekonos Worker is live.", {
-      headers: {"Content-Type": "text/plain; charset=utf-8"},
-    });
-  },
-};
