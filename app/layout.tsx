7eeb8f62d8c23cebff0e5b6415dcc06cdfbe3893
haidurqureshi <haidurqureshi@gmail.com>
2026-06-20 17:08:09 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-zinc-50 font-sans dark:bg-black">
+        
         {children}
 
         <footer className="flex flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black text-center py-4 ">
